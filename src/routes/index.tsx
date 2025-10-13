bf037b4c367f05ab31618ef7d19cc57972ef539d
masud2005 <masud20012005@gmail.com>
2025-10-14 00:35:06 +0600

    dark mode implement and login, register page design

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 
 import App from "@/App";
 import About from "@/pages/About";
+import Login from "@/pages/Login";
+import Register from "@/pages/Register";
 import { createBrowserRouter } from "react-router";
 
 export const router = createBrowserRouter([
@@ -13,5 +15,14 @@ export const router = createBrowserRouter([
                 path: "about",
             },
         ],
+    },
+
+    {
+        Component: Login,
+        path: "/login"
+    },
+    {
+        Component: Register,
+        path: "/register"
     },
 ]);
